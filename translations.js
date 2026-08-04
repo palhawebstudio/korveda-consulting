359c013e22cdb1287fe35719d8e8c610f63ba99f
palhawebstudio <palhawebstudio@gmail.com>
2026-08-04 21:30:53 +0100

    Update translations.js

--- translations.js
+++ translations.js
@@ -1,1 +1,36 @@
+const translations = {
 
+    en: {
+
+        "nav-about": "About",
+        "nav-services": "Services",
+        "nav-markets": "Global Reach",
+        "nav-contact": "Contact",
+
+        "contact-button": "Contact Us"
+
+    },
+
+    pt: {
+
+        "nav-about": "Sobre",
+        "nav-services": "Serviços",
+        "nav-markets": "Presença Global",
+        "nav-contact": "Contacto",
+
+        "contact-button": "Contacte-nos"
+
+    },
+
+    es: {
+
+        "nav-about": "Sobre Nosotros",
+        "nav-services": "Servicios",
+        "nav-markets": "Alcance Global",
+        "nav-contact": "Contacto",
+
+        "contact-button": "Contáctenos"
+
+    }
+
+};
