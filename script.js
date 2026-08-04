16b844be6febbab20631f47f9cb4e5197a8e8462
palhawebstudio <palhawebstudio@gmail.com>
2026-08-04 20:43:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+window.addEventListener("scroll", function () {
 
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 80) {
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
