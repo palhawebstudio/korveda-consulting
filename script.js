7f856795c2ebbe65fcfa29bdd1d84176f69271f6
palhawebstudio <palhawebstudio@gmail.com>
2026-08-06 18:37:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ window.addEventListener("scroll", function () {
     }
 
 });
+// =========================================
+// LANGUAGE SYSTEM
+// =========================================
+
+let currentLanguage = localStorage.getItem("language") || "en";
+
+function changeLanguage(language) {
+
+    currentLanguage = language;
+
+    localStorage.setItem("language", language);
+
+    document.querySelectorAll(".lang-btn").forEach(btn => {
+
+        btn.classList.remove("active");
+
+    });
+
+    document
+        .querySelector(`[data-language="${language}"]`)
+        .classList.add("active");
+
+    document.querySelectorAll("[data-lang]").forEach(element => {
+
+        const key = element.dataset.lang;
+
+        if (translations[language][key]) {
+
+            element.innerHTML = translations[language][key];
+
+        }
+
+    });
+
+}
+
+document.querySelectorAll(".lang-btn").forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        changeLanguage(button.dataset.language);
+
+    });
+
+});
+
+changeLanguage(currentLanguage);
